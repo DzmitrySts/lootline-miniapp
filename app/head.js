5835f7299ac7bd707142a228a14f4d22a998571d
DzmitrySts <dimastepanenko89@gmail.com>
2025-11-30 01:24:33 +0300

    Add Head component with title and meta tags

---
+++ app/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>LOOTLINE</title>
+      <meta name="description" content="Digital drops, криптоподарки, лутбоксы" />
+    </>
+  );
+}
