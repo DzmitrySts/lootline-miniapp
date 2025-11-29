bdb9679c0613e604d190fdaee2e0a2118dec7de6
DzmitrySts <dimastepanenko89@gmail.com>
2025-11-30 02:05:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone" // или можно убрать, если не нужен
+  output: "export"
 };
 
 module.exports = nextConfig;
