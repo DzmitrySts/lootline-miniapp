37e0d0bfa1e77958a8e0c6dffbca82eb9d45fdcf
DzmitrySts <dimastepanenko89@gmail.com>
2025-11-30 02:29:38 +0300

    Create About.jsx

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 50 }}>
+      <h2>About LOOTLINE</h2>
+      <p>Проект цифровых дропов и NFT-призов.</p>
+    </div>
+  );
+}
