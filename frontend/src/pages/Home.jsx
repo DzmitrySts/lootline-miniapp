27e00f25ca602cd4faf204a1a67d609799b83e84
DzmitrySts <dimastepanenko89@gmail.com>
2025-11-30 02:28:39 +0300

    Add Home component with title and description

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 50 }}>
+      <h1>LOOTLINE</h1>
+      <p>Цифровые дропы. Открывай лутбоксы и получай призы!</p>
+    </div>
+  );
+}
