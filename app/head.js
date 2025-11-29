e9474f760491e422ccddf884491b4c4ea5f6ecb0
DzmitrySts <dimastepanenko89@gmail.com>
2025-11-30 01:57:16 +0300

    Delete app directory

--- app/head.js
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>LOOTLINE</title>
-      <meta name="description" content="Digital drops, криптоподарки, лутбоксы" />
-    </>
-  );
-}
